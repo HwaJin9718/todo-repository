f4a11431229a040ffff181274412754b273d47cc
HwaJin9718 <165185641+HwaJin9718@users.noreply.github.com>
2026-01-20 17:51:56 +0900

    deploy.sh file modify

--- deploy.sh
+++ deploy.sh
@@ -1,29 +1,43 @@
-#!/bin/bash
+# !/bin/bash
+# 해당 파일은 cd ~ 로 이동해서 vi deploy.sh 진행
+# cd ~ : 명령어 입력 시 /home/ubuntu로 이동함
 
-set -e
-echo "1. 프로젝트 폴더로 이동"
-export PATH="$HOME/.local/bin:$PATH"
-
-echo "2. 최신 코드 pull"
+# 강사님 버전
+cd /home/ubuntu/workspace/todo-repository
 git pull origin main
+pkill -f "uvicorn main:app" # or pkill uvicorn
 
-echo "3. 기존 uvicorn 프로세스 종료"
-if [ -f app.pid ]; then
-  PID=$(cat app.pid)
-  if ps -p $PID > /dev/null; then
-    echo "Stopping process $PID"
-    kill $PID
-  fi
-  rm -f app.pid
-else
-  echo "No PID file found"
-fi
+# uvicorn이 종료되는데 시간 소요, 5초 sleep 했다가 fastapi 다시 시작
+sleep 5 
 
-uv sync
+nohup uv run uvicorn main:app --host 0.0.0.0 --port 8000 \
+  > /dev/null 2>&1 < /dev/null &
 
-echo "4. 서버 백그라운드 실행"
-nohup uv run uvicorn main:app --host 0.0.0.0 --port 8000 > /dev/null 2>&1 < /dev/null &
+# set -e
+# echo "1. 프로젝트 폴더로 이동"
+# export PATH="$HOME/.local/bin:$PATH"
 
-echo $! > app.pid
+# echo "2. 최신 코드 pull"
+# git pull origin main
 
-echo "FastAPI server started with PID: $(cat app.pid)"
+
+# echo "3. 기존 uvicorn 프로세스 종료"
+# if [ -f app.pid ]; then
+#   PID=$(cat app.pid)
+#   if ps -p $PID > /dev/null; then
+#     echo "Stopping process $PID"
+#     kill $PID
+#   fi
+#   rm -f app.pid
+# else
+#   echo "No PID file found"
+# fi
+
+# uv sync
+
+# echo "4. 서버 백그라운드 실행"
+# nohup uv run uvicorn main:app --host 0.0.0.0 --port 8000 > /dev/null 2>&1 < /dev/null &
+
+# echo $! > app.pid
+
+# echo "FastAPI server started with PID: $(cat app.pid)"
